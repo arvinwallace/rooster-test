782bfb592817584e13a50142a1690f5639fac350
Arvin Wallace <arvinwallace@gmail.com>
2021-04-28 11:03:49 -0500

    style pics

--- src/dog-breeds/index.js
+++ src/dog-breeds/index.js
@@ -7,7 +7,7 @@ function DogBreeds(){
 
   useEffect(() => {
     fetch("https://dog.ceo/api/breeds/list/all")
-    .then((res) => res.json())
+    .then(res => res.json())
     .then(d => flatDawgs(d.message))
     .then(dawgs => {
           Promise.all(dawgs.map(d => {
@@ -23,19 +23,29 @@ function DogBreeds(){
     .catch(e => console.log('errorss',e))
   },[])
 
+  const filterDogs = string => {
+    console.log("strang", string)
+  }
+
   if(!pics.length){
     return (
       <h1>Here come the breeds</h1>
     )
   }
   return (
+          <>
+          <div style={{margin:"0px auto"}}>Search</div>
     <div style={{display:"flex",flexWrap:"wrap", justifyContent:"center"}}>
       {pics.map(dog => {
         return (
+          <div>
+            <p style={{alignSelf:"center", width:"80%",margin:"25px auto 0px auto",padding:"7px 0px", borderTop:"1px solid black"}}>{dog.name}</p>
             <img style={{height:"300px", width:"300px", objectFit:"cover", border:"10px solid black"}} src={dog.pic} alt={dog.name}/>
+          </div>
         )
       })}
     </div>
+          </>
   )
 }
 
